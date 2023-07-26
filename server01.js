3214103248b9d08fe2ae6f576e2a83adbbe0cf7f
Tanaka Seigo <seigo@1ft-seabass.jp>
2023-07-26 16:53:46 +0900

    更新

--- server01.js
+++ server01.js
@@ -1,20 +1,13 @@
+// Express ライブラリの呼び出し
 const express = require('express');
+// Express ライブラリからサーバーの仕組みを app 変数として呼び出す
 const app = express();
 
+// public フォルダ内にあるファイルはパスが一致していると呼びだせます
+// /index.html や / の場合は public フォルダ内の index.html が表示されます
 app.use(express.static(__dirname + '/public'));
 
-// bodyParser
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/api/get/sample', (req, res) => {
-  res.send('GET OK!')
-});
-
-app.post('/api/post/sample', (req, res) => {
-  res.send('POST OK!')
-});
-
+// サーバーを 8080 ポートで起動してログを出力
 app.listen(process.env.PORT || 8080, () => {
   console.log("server01 start!");
   console.log(`app listening at http://localhost:${process.env.PORT || 8080}`)
--- server02.js
+++ server02.js
@@ -1,21 +1,29 @@
+// Express ライブラリの呼び出し
 const express = require('express');
+// Express ライブラリからサーバーの仕組みを app 変数として呼び出す
 const app = express();
 
+// public フォルダ内にあるファイルはパスが一致していると呼びだせます
+// /index.html や / の場合は public フォルダ内の index.html が表示されます
 app.use(express.static(__dirname + '/public'));
 
-// bodyParser
+// bodyParser の設定
+// ※現在はひとまず置いておけばよい処理
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
+// /api/get/sample というパスで GET リクエストでアクセスすると GET OK! が表示されます
 app.get('/api/get/sample', (req, res) => {
-  res.json({"result":"GET OK!"})
+  res.send('GET OK!')
 });
 
+// /api/post/sample というパスで POST リクエストでアクセスすると POST OK! が表示されます
 app.post('/api/post/sample', (req, res) => {
-  res.json({"result":"POST OK!"})
+  res.send('POST OK!')
 });
 
+// サーバーを 8080 ポートで起動してログを出力
 app.listen(process.env.PORT || 8080, () => {
-  console.log("server02 start!");
+  console.log("server01 start!");
   console.log(`app listening at http://localhost:${process.env.PORT || 8080}`)
 })
