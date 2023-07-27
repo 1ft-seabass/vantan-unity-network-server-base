c31b7c7f6297e590efd018f502493bf484202c65
Tanaka Seigo <seigo@1ft-seabass.jp>
2023-07-27 15:29:23 +0900

    更新

--- server03.js
+++ server03.js
@@ -1,20 +1,25 @@
+// Express ライブラリの呼び出し
 const express = require('express');
+// Express ライブラリからサーバーの仕組みを app 変数として呼び出す
 const app = express();
 
+// public フォルダ内にあるファイルはパスが一致していると呼びだせます
+// /index.html や / の場合は public フォルダ内の index.html が表示されます
 app.use(express.static(__dirname + '/public'));
 
-// bodyParser
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/api/get/sample', (req, res) => {
-  res.json({"result":"GET OK!"})
+// /api/post/title というパスで POST リクエストでアクセスすると「こんにちは！ようこそ！」というテキストが取得できます
+app.post('/api/post/title', (req, res) => {
+  console.log('あいさつメッセージ返答！');
+  res.send('こんにちは！ようこそ！')
 });
 
-app.post('/api/post/sample', (req, res) => {
-  res.json({"result":"POST OK!"})
+// /api/post/title というパスで POST リクエストでアクセスすると「こんにちは！ようこそ！」というテキストが取得できます
+app.post('/api/post/add_point', (req, res) => {
+  console.log('ポイント加算設定返答！');
+  res.send('1')
 });
 
+// サーバーを 8080 ポートで起動してログを出力
 app.listen(process.env.PORT || 8080, () => {
   console.log("server02 start!");
   console.log(`app listening at http://localhost:${process.env.PORT || 8080}`)
