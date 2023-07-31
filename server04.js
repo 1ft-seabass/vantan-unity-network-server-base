f7a703a362e042115871f85ab13dbb1d91573a43
Tanaka Seigo <seigo@1ft-seabass.jp>
2023-07-31 15:51:47 +0900

    更新

--- server04.js
+++ server04.js
@@ -3,14 +3,12 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.post('/api/post/json', (req, res) => {
-
+app.get('/api/get/json1', (req, res) => {
   // 送るデータを JavaScript のオブジェクトで作る
   const responseData = {
-    "welcome":"こんにちは！ようこそ！",
-    "add_point":1,
+    "name":"Box1"
   };
-  // res.json はオブジェクトをJSON 形式で返答する
+  // res.json はオブジェクトを JSON 形式で返答します
   // 以前のテキスト返答は res.send(文字列) を使っていました
   res.json(responseData)
 });
